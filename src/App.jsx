4fce8ed12fafcd02d4e40a7a427263bd6a410e55
Kayo <91280312+okayosilva@users.noreply.github.com>
2025-01-02 12:46:59 -0300

    refactor: update App component to display random messages and enhance UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return <div className="bg-black text-white">Hello World</div>;
-}
+// eslint-disable-next-line react/prop-types
+const ItemMessage = ({ message }) => <p className="text-white">{message}</p>;
 
-export default App;
+export const App = () => {
+    const [messageList] = useState(["Hello World", "Hi", "How are you?"]);
+    const [currentMessage, setCurrentMessage] = useState(messageList[0]);
+
+    const handleChangeMessage = () => {
+        setCurrentMessage(
+            messageList[Math.floor(Math.random() * messageList.length)],
+        );
+    };
+
+    return (
+        <div className="flex h-screen flex-col items-center justify-center gap-4 bg-blue-500 text-2xl text-white">
+            <ItemMessage message={currentMessage} />
+            <button
+                onClick={handleChangeMessage}
+                className="rounded-md bg-green-500 p-2 text-xl font-semibold text-white"
+            >
+                Change Message
+            </button>
+        </div>
+    );
+};
